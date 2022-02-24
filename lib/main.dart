8389a95ea09a5ecedc907181eb6c2a663c765fd4
dushyant <sharma-dushyant>
2022-02-24 23:52:14 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/screens/sign_up_screen.dart';
+import 'package:instagram_flutter/utils/colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyBEMf3PvHrQDOKs0Afwi0HsUoTJxkL6em4",
+        appId: "1:708891892720:web:f6c4bf4f5c90a6bd191b35",
+        messagingSenderId: "708891892720",
+        projectId: "instagram-clone-e768a",
+        storageBucket: "instagram-clone-e768a.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram Clone',
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+      // home: const ResponsiveLayout(
+      //     webScreenLayout: WebScreenLayout(),
+      //     mobileScreenLayout: MobileScreenLayout()));
+      home: const SignUpScreen(),
+    );
+  }
+}
